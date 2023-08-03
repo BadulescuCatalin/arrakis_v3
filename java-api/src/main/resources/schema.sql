ecfe946c5474bf0a44f253187f56a5c8b45b93a1
deepika57 <amruthadeepika@gmail.com>
2023-08-03 02:05:14 +0100

    This is database with insert data

--- java-api/src/main/resources/schema.sql
+++ java-api/src/main/resources/schema.sql
@@ -1,15 +1,17 @@
 
 
 CREATE TABLE [IF NOT EXISTS] security (
-    bond_id INT AUTO_INCREMENT NOT NULL ,
+    security_id INT NOT NULL ,
+    isin VARCHAR(500) NOT NULL,
     bond_name VARCHAR(250) NOT NULL,
     cusip    VARCHAR(500) ,
-    isin VARCHAR(500) NOT NULL ,
     bond_type CHAR(50) NOT NULL,
     bond_status CHAR(50) NOT NULL,
     bond_maturity_date DATE,
     bond_currency CHAR(50) NOT NULL,
-    PRIMARY KEY(isin)
+    issuer_name VARCHAR(250) NOT NULL,
+    trade_id INT,
+    PRIMARY KEY(security_id,isin),
     FOREIGN KEY (trade_id) REFERENCES trade(trade_id)
 );
 
@@ -17,11 +19,14 @@ CREATE TABLE [IF NOT EXISTS] security (
 CREATE TABLE [IF NOT EXISTS] book (
     book_id INT AUTO_INCREMENT NOT NULL [PRIMARY KEY],
     book_name VARCHAR(250) NOT NULL,
-    FOREIGN KEY (trade_id) REFERENCES trade(trade_id)
+    isin VARCHAR(500),
+    FOREIGN KEY (isin) REFERENCES security(isin)
 );
 
+
+
 CREATE TABLE [IF NOT EXISTS] trade (
-    trade_id INT NOT NULL [PRIMARY KEY],
+    trade_id INT AUTO_INCREMENT NOT NULL [PRIMARY KEY],
     trade_type CHAR(50) NOT NULL,
     trade_currency CHAR(50) NOT NULL,
     trade_quantity INT NOT NULL,
@@ -29,6 +34,7 @@ CREATE TABLE [IF NOT EXISTS] trade (
     trade_status CHAR(50) NOT NULL,
     trade_date DATE,
     trade_unit_price FLOAT NOT NULL,
+    isin VARCHAR(500),
     FOREIGN KEY (isin) REFERENCES security(isin)
 );
 
